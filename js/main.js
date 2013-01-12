5f67d9fd641c50943b3f09cffc9e7cc1c725c9ee
Barbara Szczygieł <barbara.szczygiel@gmail.com>
2013-01-12 19:52:44 +0100

    fading out

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(document).ready( function() {
     setTimeout( function() {
       $("#woda").slideDown(1000, function(){
         $("#dasein").slideDown(1000, function(){
-          $("#kwiatek").slideDown(1000);
+          $("#kwiatek").slideDown(1000, function(){
+            $("#fadebox").fadeOut("slow");
+          });
         });
       });
     },1000);
