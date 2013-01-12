530a516841961b78d042eb8911900a52249045df
Barbara Szczygieł <barbara.szczygiel@gmail.com>
2013-01-12 18:19:47 +0100

    onclick interaction, water animation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready( function() {
+  $("#konewka").click(function() {
+    $(this).addClass("rotate30");
+    setTimeout( function() {
+      $("#woda").slideDown(1000, function(){
+        $(this).css('display','block');
+      });
+    },1000);
+  });
+});
