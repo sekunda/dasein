a8ecdcde5cf9c445fbaeef53eaf7480fcea188d8
Przemysław Dąbek <przemyslaw.dabek@gmail.com>
2013-01-14 13:41:23 +0100

    replaced ids by classes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready( function() {
-  $("#konewka").click(function() {
+  $(".konewka").click(function() {
     $(this).addClass("rotate30");
     setTimeout( function() {
-      $("#woda").slideDown(1000, function(){
-        $("#dasein").slideDown(1000, function(){
-          $("#kwiatek").slideDown(1000, function(){
-            $("#fadebox").fadeOut("slow");
+      $(".woda").slideDown(1000, function(){
+        $(".dasein").slideDown(1000, function(){
+          $(".kwiatek").slideDown(1000, function(){
+            $(".fadebox").fadeOut("slow");
           });
         });
       });
