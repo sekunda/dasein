07d361b21bf1c8899f5c7b3abcf9031da14b874b
Barbara Szczygieł <barbara.szczygiel@gmail.com>
2013-01-14 15:36:00 +0100

    adding browser vendor prefixes + minor animation refinements

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ $(document).ready( function() {
   $("#konewka").click(function() {
     $(this).addClass("rotate30");
     setTimeout( function() {
-      $("#woda").slideDown(1000, function(){
-        $("#dasein").slideDown(1000, function(){
-          $("#kwiatek").slideDown(1000, function(){
-            $("#fadebox").fadeOut("slow");
+      $("#woda").slideDown(1000, function() {
+        $("#dasein").slideDown(2000, function() {
+          $("#kwiatek").slideDown(1000, function() {
+            $("#fadebox").fadeOut(3000, function() {
+              $("#kwiatek").addClass("object");
+              $("#kwiatek").addClass("rotate360");
+            });
           });
         });
       });
